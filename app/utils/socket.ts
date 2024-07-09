466cbd846ca5c5495419a4749aed6652389ce902
Pratham singh <121001pratham@gmail.com>
2024-07-09 20:44:08 +0530

    new code

--- app/utils/socket.ts
+++ app/utils/socket.ts
@@ -1,4 +1,5 @@
+'use client'
 import {io} from 'socket.io-client' 
-export const base_url = process.env.BASE_URL || 'http://localhost:5000'
+export const base_url = process.env.NEXT_PUBLIC_BASE_URL
 const socket = io(base_url); // Replace with your server URL
 export default socket;
