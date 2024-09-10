fc5130fe5c4cc57a0b3b2e7a03c2713043bb0960
Pratham singh <121001pratham@gmail.com>
2024-09-10 17:43:09 +0530

    sovket

--- app/utils/socket.ts
+++ app/utils/socket.ts
@@ -2,5 +2,8 @@
 import {io} from 'socket.io-client' 
 export const base_url = process.env.NEXT_PUBLIC_BASE_URL
 console.log(base_url)
-const socket = io(base_url); // Replace with your server URL
+const socket = io(base_url,{
+    transports:['websocket'],
+    withCredentials:true
+}); // Replace with your server URL
 export default socket;
